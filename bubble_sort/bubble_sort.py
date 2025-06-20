b9902f4d33e445d4bf7ab78339a70a28149e77ec
SajidaDahir <sajidadahirsheikh@gmail.com>
2025-06-20 10:01:32 +0300

    Sorting

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,11 @@
-def bubble_sort(unsorted_list):
+def bubble_sort(arr):
     # TODO: Implement bubble sort
-    pass
+    n = len(arr)    #no. of elements in the array
+    for i in range(n):      #loop continues for n times
+        for j in range(0,n-i-1):    #enables us to compare unsorted elements only
+            if arr[j]>arr[j+1]: #current index is greater than index after
+                arr[j], arr[j+1] = arr[j+1], arr[j]     #swaps the two elements
+    return arr
+
+input_list = [20,9,17,12,44]
+print(bubble_sort(input_list.copy()))
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,13 @@
 def selection_sort(arr):
     # TODO: Implement selection sort
-    pass
+    n = len(arr)
+    for i in range(n):  #loops through each element
+        min_idx = i     #assumes current element is the smallest
+        for j in range(i+1, n): #checks for smaller element in each index
+            if arr[j] < arr[min_idx]: #if found
+                min_idx = j         #that element becomes the smallest
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]    #swaps the current index with the smaller one
+    return arr
+
+input_list = [33,10,88,54,20]
+print(selection_sort(input_list.copy()))
